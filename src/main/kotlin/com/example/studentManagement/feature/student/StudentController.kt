987b4cd0059ad152e1978d90cefee53d48e2ff3b
gioitv <gioitv@natajsc.com.vn>
2022-03-22 06:14:25 +0700

    fix cors localhost:4200

--- src/main/kotlin/com/example/studentManagement/feature/student/StudentController.kt
+++ src/main/kotlin/com/example/studentManagement/feature/student/StudentController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin(origins = ["http://localhost:8080"], maxAge = 3600)
+@CrossOrigin(origins = ["http://localhost:4200"], maxAge = 3600)
 @RequestMapping("/student")
 class StudentController(
     @Autowired val studentServices: IStudentServices
