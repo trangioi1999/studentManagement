9863dd784794da163cb5ab94d3fc710166d2c093
trangioi1999 <trangioi205@gmail.com>
2022-03-21 21:04:07 +0700

    add CrossOrigin for localhost

--- src/main/kotlin/com/example/studentManagement/feature/student/StudentController.kt
+++ src/main/kotlin/com/example/studentManagement/feature/student/StudentController.kt
@@ -7,37 +7,31 @@ import com.example.studentManagement.models.UpdateTime
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping()
+@CrossOrigin(origins = ["http://localhost:8080"], maxAge = 3600)
+@RequestMapping("/student")
 class StudentController(
     @Autowired val studentServices: IStudentServices
 ) {
     val logger = LoggerFactory.getLogger(StudentController::class.java)!!
 
-    @PostMapping("/student")
+    @PostMapping
     fun createStudent(@RequestBody studentInfo: StudentDTO) : ResponseEntity<Any> {
         return ResponseEntity.ok(studentServices.createStudent(studentInfo))
     }
-    @GetMapping("/student")
+    @GetMapping
     fun getStudentList() : ResponseEntity<Any> {
         return ResponseEntity.ok(studentServices.getStudentList())
     }
 
-    @PutMapping("/student")
+    @PutMapping
     fun updateStudentInfo(@RequestBody studentInfo: StudentUpdateInfoDTO) : ResponseEntity<Any> {
         return ResponseEntity.ok(UpdateTime(studentServices.updateStudentInfo(studentInfo)!!.seconds))
     }
 
-    @DeleteMapping("/student")
+    @DeleteMapping
     fun disableStudent(@RequestParam id: String) : ResponseEntity<Any> {
         return ResponseEntity.ok(UpdateTime(studentServices.disableStudent(id)!!.seconds))
     }
